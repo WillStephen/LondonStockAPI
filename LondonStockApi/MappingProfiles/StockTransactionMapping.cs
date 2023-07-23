02aa8d1ea99146c5964e063676a2dccde2f2fe60
Will Stephen <will.stephen1995@gmail.com>
2023-07-23 12:41:02 +0100

    Add AutoMapper to project

---
+++ LondonStockApi/MappingProfiles/StockTransactionMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LondonStockApi.Data.Entities;
+using LondonStockApi.DTO;
+
+namespace LondonStockApi.Profiles
+{
+    public class StockTransactionMapping : Profile
+    {
+        public StockTransactionMapping()
+        {
+            CreateMap<StockTransactionDTO, StockTransaction>();
+        }
+    }
+}
--- LondonStockApi/Program.cs
+++ LondonStockApi/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddSwaggerGen(options =>
 
 builder.Services.AddScoped<IStockContext, StockContext>();
 
+builder.Services.AddAutoMapper(typeof(Program));
+
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
