07f53d6c81d93bb1305ff05cfba19a8f02d3ba90
Will Stephen <will.stephen1995@gmail.com>
2023-07-23 12:41:28 +0100

    Implement POST controller method - log, map to entity and persist with EF Core

--- LondonStockApi/Controllers/StockTransactionController.cs
+++ LondonStockApi/Controllers/StockTransactionController.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using LondonStockApi.Data;
+using LondonStockApi.Data.Entities;
 using LondonStockApi.DTO;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,11 +11,16 @@ namespace LondonStockApi.Controllers
     public class StockTransactionController : ControllerBase
     {
         private readonly IStockContext stockContext;
+        private readonly IMapper mapper;
         private readonly ILogger<StockTransactionController> logger;
 
-        public StockTransactionController(IStockContext stockContext, ILogger<StockTransactionController> logger)
+        public StockTransactionController(
+            IStockContext stockContext,
+            IMapper mapper,
+            ILogger<StockTransactionController> logger)
         {
             this.stockContext = stockContext;
+            this.mapper = mapper;
             this.logger = logger;
         }
 
@@ -23,9 +30,14 @@ namespace LondonStockApi.Controllers
         [HttpPost]
         public async Task<ActionResult<StockTransactionDTO>> Post([FromBody] StockTransactionDTO stockTransaction)
         {
-            this.logger.LogInformation("Received stock transaction {stockTransaction}", stockTransaction);
+            this.logger.LogInformation("Received stock transaction from client {stockTransaction}", stockTransaction);
 
-            // TODO: map DTO to entity and commit to DB
+            StockTransaction entity = this.mapper.Map<StockTransaction>(stockTransaction);
+
+            await this.stockContext.StockTransactions.AddAsync(entity);
+            await this.stockContext.SaveChangesAsync();
+
+            this.logger.LogInformation("Persisted stock transaction {stockTransaction}", stockTransaction);
 
             return StatusCode(201);
         }
