1c515afde5340e2a83e662ec0151ad13a16bfd1f
Will Stephen <will.stephen1995@gmail.com>
2023-07-23 12:38:18 +0100

    Add StockTransactionControllerUnitTests

---
+++ LondonStockApi.UnitTest/StockTransactionControllerUnitTests.cs
@@ -0,0 +1,98 @@
+using AutoFixture;
+using AutoMapper;
+using LondonStockApi.Controllers;
+using LondonStockApi.Data;
+using LondonStockApi.Data.Entities;
+using LondonStockApi.DTO;
+using LondonStockApi.Profiles;
+using LondonStockApi.UnitTest.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Net;
+
+namespace LondonStockApi.UnitTest
+{
+    [TestClass]
+    public class StockTransactionControllerUnitTests
+    {
+        private readonly Fixture fixture = new Fixture();
+
+        private readonly Mock<IStockContext> mockStockContext;
+        private readonly Mock<DbSet<StockTransaction>> mockStockTransactionSet;
+        private readonly Mock<ILogger<StockTransactionController>> mockLogger;
+
+        private readonly StockTransactionController sut;
+
+        public StockTransactionControllerUnitTests()
+        {
+            this.mockStockContext = new Mock<IStockContext>();
+            this.mockStockTransactionSet = new Mock<DbSet<StockTransaction>>();
+            this.mockStockContext.Setup(msc => msc.StockTransactions).Returns(this.mockStockTransactionSet.Object);
+            this.mockLogger = new Mock<ILogger<StockTransactionController>>();
+
+            IMapper mapper = new MapperConfiguration(mc => mc.AddProfile(new StockTransactionMapping())).CreateMapper();
+
+            this.sut = new StockTransactionController(this.mockStockContext.Object, mapper, this.mockLogger.Object);
+        }
+
+        #region Test methods
+
+        [TestMethod]
+        public async Task Post_ContextHasNoIssues_Returns201()
+        {
+            // Arrange
+            var dto = this.fixture.Create<StockTransactionDTO>();
+
+            // Act
+            ActionResult<StockTransactionDTO> result = await this.sut.Post(dto);
+
+            // Assert
+            this.mockLogger.VerifyLog(LogLevel.Information, messageSubstring: "Received stock transaction from client");
+            this.mockLogger.VerifyLog(LogLevel.Information, messageSubstring: "Persisted stock transaction");
+
+            this.mockStockContext.Verify(
+                sc => sc.StockTransactions.AddAsync(It.Is<StockTransaction>(st => ValuesEqual(dto, st)), default),
+                Times.Once);
+            this.mockStockContext.Verify(sc => sc.SaveChangesAsync(default), Times.Once);
+            this.mockStockContext.VerifyNoOtherCalls();
+
+            Assert.IsNull(result.Value);
+            StatusCodeResult? statusCodeResult = result.Result as StatusCodeResult;
+            Assert.IsNotNull(statusCodeResult);
+            Assert.AreEqual((int)HttpStatusCode.Created, statusCodeResult.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task Post_ContextThrows_Uncaught()
+        {
+            // Arrange
+            var dto = this.fixture.Create<StockTransactionDTO>();
+            this.mockStockContext
+                .Setup(msc => msc.SaveChangesAsync(default))
+                .Throws<InvalidOperationException>();
+
+            // Act + Assert
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(() => this.sut.Post(dto));
+
+            this.mockLogger.VerifyLog(LogLevel.Information, messageSubstring: "Received stock transaction from client");
+            this.mockLogger.VerifyNoOtherCalls();
+        }
+
+        #endregion Test methods
+
+        #region Support methods
+
+        private static bool ValuesEqual(StockTransactionDTO dto, StockTransaction entity)
+        {
+            return dto.Ticker == entity.Ticker
+                && dto.PriceInSterling == entity.PriceInSterling
+                && dto.NumberOfShares == entity.NumberOfShares
+                && dto.BrokerId == entity.BrokerId;
+        }
+
+        #endregion
+    }
+}
---
+++ LondonStockApi.UnitTest/Utils/MockLoggerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace LondonStockApi.UnitTest.Utils
+{
+    public static class MockLoggerExtensions
+    {
+        public static void VerifyLog<T>(this Mock<ILogger<T>> mockLogger, LogLevel logLevel, string messageSubstring)
+        {
+            mockLogger.Verify(l => l.Log(
+                logLevel,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, _) => v.ToString().Contains(messageSubstring)),
+                It.IsAny<Exception>(),
+                It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)));
+        }
+    }
+}
