234c23ba72069cefbd03219d27a500b9efce202d
Will Stephen <will.stephen1995@gmail.com>
2023-07-23 20:52:13 +0100

    Add methods to query stock prices to StockContext using stored procs

--- LondonStockApi/Data/IStockContext.cs
+++ LondonStockApi/Data/IStockContext.cs
@@ -9,6 +9,27 @@ namespace LondonStockApi.Data
         /// <summary>The store of <see cref="StockTransaction"/> objects.</summary>
         DbSet<StockTransaction> StockTransactions { get; }
 
+        /// <summary>
+        /// Try to retrieve a quote for a single ticker.
+        /// </summary>
+        /// <param name="ticker">The ticker to search for.</param>
+        /// <returns>A <see cref="bool"/> representing whether the value was in the database, and a <see cref="StockQuote"/>
+        /// (<see langword="null"/> if not found).</returns>
+        Task<(bool Found, StockQuote? Quote)> TryGetStockQuote(string ticker);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tickers">The tickers to search for.</param>
+        /// <returns>A <see cref="bool"/> representing whether the value was in the DB, and <see cref="StockQuote"/>s 
+        /// (<see langword="null"/> if not found).</returns>
+        Task<(bool FoundAll, IEnumerable<StockQuote>? Quotes)> TryGetStockQuotes(string[] tickers);
+
+        /// <summary>
+        /// Get quotes for all tickers in the database.
+        /// </summary>
+        Task<IEnumerable<StockQuote>> GetAllStockQuotes();
+
         /// <inheritdoc cref="DbContext.SaveChangesAsync(CancellationToken)"/>
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
--- LondonStockApi/Data/StockContext.cs
+++ LondonStockApi/Data/StockContext.cs
@@ -1,5 +1,7 @@
 ﻿using LondonStockApi.Data.Entities;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace LondonStockApi.Data
 {
@@ -8,15 +10,83 @@ namespace LondonStockApi.Data
     {
         private readonly IConfiguration configuration;
 
+        /// <inheritdoc/>
+        public DbSet<StockTransaction> StockTransactions { get; set; }
+
+        private DbSet<StockQuote> StockQuotes { get; set; }
+
         public StockContext(IConfiguration configuration)
         {
             this.configuration = configuration;
         }
 
+        /// <inheritdoc/>
+        public async Task<(bool Found, StockQuote? Quote)> TryGetStockQuote(string ticker)
+        {
+            IEnumerable<StockQuote> quotes = await this.StockQuotes
+                .FromSqlInterpolated($"GetPriceForTicker {ticker}").ToListAsync();
+
+            if (!quotes.Any())
+            {
+                return (false, null);
+            }
+
+            if (quotes.Count() > 1)
+            {
+                throw new InvalidDataException("Expected no more than one result from the database.");
+            }
+
+            return (true, quotes.Single());
+        }
+
+        /// <inheritdoc/>
+        public async Task<(bool FoundAll, IEnumerable<StockQuote>? Quotes)> TryGetStockQuotes(string[] tickers)
+        {
+            SqlParameter tickerQueryParameter = CreateTickerQueryParameter(tickers);
+
+            var quotes = await this.StockQuotes
+                .FromSqlInterpolated($"GetPriceForTickers {tickerQueryParameter}").ToListAsync();
+
+            // if any of the requested tickers are missing in the results, fail the whole operation
+            // ensures the erroneous client code doesn't fail silently
+            if (quotes.Count < tickers.Length)
+            {
+                return (false, null);
+            }
+
+            if (quotes.Count > tickers.Length)
+            {
+                throw new InvalidDataException("Expected result row count to be less than or equal to ticker count.");
+            }
+
+            return (true, quotes);
+        }
+
+        /// <inheritdoc/>
+        public async Task<IEnumerable<StockQuote>> GetAllStockQuotes() =>
+            await this.StockQuotes.FromSqlInterpolated($"GetPriceForAllTickers").ToListAsync();
+
         protected override void OnConfiguring(DbContextOptionsBuilder options) => 
             options.UseSqlServer(this.configuration.GetConnectionString("LondonStockApi"));
 
-        /// <inheritdoc/>
-        public DbSet<StockTransaction> StockTransactions { get; set; }
+        #region Support methods
+
+        private SqlParameter CreateTickerQueryParameter(string[] tickers)
+        {
+            // create a temporary table to pass values to the stored procedure
+            var tickerQueryTable = new DataTable();
+            tickerQueryTable.Columns.Add("Ticker", typeof(string));
+            foreach (var ticker in tickers)
+            {
+                tickerQueryTable.Rows.Add(ticker);
+            }
+
+            var parameter = new SqlParameter("@TickerQuery", SqlDbType.Structured);
+            parameter.Value = tickerQueryTable;
+            parameter.TypeName = "dbo.TickerQuery";
+            return parameter;
+        }
+
+        #endregion Support methods
     }
 }
