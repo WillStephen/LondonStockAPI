8d18abf9edf0195e01cf3f6520eee162a8e9c148
Will Stephen <will.stephen1995@gmail.com>
2023-07-23 20:51:25 +0100

    Add mapping between stock quote and entity

---
+++ LondonStockApi/MappingProfiles/StockQuoteMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LondonStockApi.Data.Entities;
+using LondonStockApi.DTO;
+
+namespace LondonStockApi.MappingProfiles
+{
+    /// <summary>Maps <see cref="StockQuote"/> to <see cref="StockQuoteDTO"/>.</summary>
+    public class StockQuoteMapping : Profile
+    {
+        public StockQuoteMapping()
+        {
+            CreateMap<StockQuote, StockQuoteDTO>();
+        }
+    }
+}
